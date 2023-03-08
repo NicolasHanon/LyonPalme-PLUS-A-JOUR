651e73b847c19d6a20f82666e8c734dc46b9ecd0
nicolashanon <nicolashanon@leschassagnes.net>
2023-03-08 16:13:06 +0100

    Modification des champs des formulaire d'entrainements et ajout d'une feuille de style simple.

--- src/planning/forms.py
+++ src/planning/forms.py
@@ -11,14 +11,30 @@ class ENTRAINEMENTForm(ModelForm):
             'DateEntrainement': forms.DateInput(attrs={'class': 'form-control'}),
             'NomPartie1': forms.TextInput(attrs={'class': 'form-control'}),
             'Partie1': forms.Textarea(attrs={'class': 'form-control'}),
-            'TempsPartie1': forms.TimeInput(attrs={'class': 'form-control'}),
+            'TempsPartie1': forms.TimeInput(attrs={'class': 'form-control', 'type': 'time'}),
             'DistancePartie1': forms.TextInput(attrs={'class': 'form-control'}),
             'NomPartie2': forms.TextInput(attrs={'class': 'form-control'}),
             'Partie2': forms.Textarea(attrs={'class': 'form-control'}),
-            'TempsPartie2': forms.TimeInput(attrs={'class': 'form-control'}),
+            'TempsPartie2': forms.TimeInput(attrs={'class': 'form-control', 'type': 'time'}),
             'DistancePartie2': forms.TextInput(attrs={'class': 'form-control'}),
             'NomPartie3': forms.TextInput(attrs={'class': 'form-control'}),
             'Partie3': forms.Textarea(attrs={'class': 'form-control'}),
-            'TempsPartie3': forms.TimeInput(attrs={'class': 'form-control'}),
+            'TempsPartie3': forms.TimeInput(attrs={'class': 'form-control', 'type': 'time'}),
             'DistancePartie3': forms.TextInput(attrs={'class': 'form-control'}),
         }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['Titre'].label = 'Titre de l\'entrainement\r'
+        self.fields['DateEntrainement'].label = 'Date de l\'entrainement'
+        self.fields['NomPartie1'].label = 'Echauffement'
+        self.fields['Partie1'].label = 'Contenu de l\'échauffement'
+        self.fields['TempsPartie1'].label = 'Durée (hh:mm)'
+        self.fields['DistancePartie1'].label = 'Distance (en m)'
+        self.fields['NomPartie2'].label = 'Intitulé de l\'exercice 1'
+        self.fields['Partie2'].label = 'Contenu de l\'exercice'
+        self.fields['TempsPartie2'].label = 'Durée (hh:mm)'
+        self.fields['DistancePartie2'].label = 'Distance (en m)'
+        self.fields['NomPartie3'].label = 'Initulé de l\'exercice 2'
+        self.fields['Partie3'].label = 'Contenu de l\'exercice'
+        self.fields['TempsPartie3'].label = 'Durée (hh:mm)'
+        self.fields['DistancePartie3'].label = 'Distance (en m)'
